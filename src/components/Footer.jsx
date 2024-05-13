db158f87b5d370b90614279cb3ef57c86d4c7d7a
Kasim Saifi <106295428+Qasimsaifi@users.noreply.github.com>
2024-05-14 00:46:10 +0530

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,57 +3,50 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-        <footer className=" md:flex bg-white dark:bg-black md:items-center md:justify-between shadow rounded-lg px-4 md:px-6 xl:px-8 py-2 md:py-4 xl:py-6 ">
-  <ul className="flex items-center flex-wrap mb-6 md:mb-0">
-    <li className='mr-4 mt-6'>
+       <footer className="bg-white  shadow dark:bg-black ">
+  <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+    <div className="sm:flex sm:items-center sm:justify-between">
       <a
-        href="/projects"
-        className="text-sm font-normal  hover:underline mr-md:mr-6"
+        href="/"
+        className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
       >
-        <img src="logo-hero.png" className="h-16 w-16" alt="" />
-      </a>
-    </li>
-    <li>
-      <a
-        href="/projects"
-        className="text-sm font-normal  hover:underline mr-4 md:mr-6"
-      >
-        Projects
-      </a>
-    </li>
- 
-    <li>
-      <a
-        href="/about"
-        className="text-sm font-normal  hover:underline mr-4 md:mr-6"
-      >
-        About
-      </a>
-    </li>
-    
-    <li>
-      <a href="/contact" className="text-sm font-normal  hover:underline">
-        Contact
-      </a>
-    </li>
-  </ul>
-  <div className="flex sm:justify-center space-x-6">
-   
-    <a href="#" className=" hover:text-gray-900">
-      <svg
-        className="h-5 w-5"
-        fill="currentColor"
-        viewBox="0 0 24 24"
-        aria-hidden="true"
-      >
-        <path
-          fillRule="evenodd"
-          d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-          clipRule="evenodd"
+        <img
+          src="logo-hero.png"
+          className="h-8"
+          alt="Flowbite Logo"
         />
-      </svg>
-    </a>
-    
+        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+          FinalStop<span className="font-extrabold text-2xl text-center mb-1 text-[#7cb40c] ">.</span>
+        </span>
+      </a>
+      <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+        <li>
+          <a href="/about" className="hover:underline me-4 md:me-6">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="/projects" className="hover:underline me-4 md:me-6">
+            projects
+          </a>
+        </li>
+        <li>
+          <a href="/contact" className="hover:underline me-4 md:me-6">
+            Contact
+          </a>
+        </li>
+       
+      </ul>
+    </div>
+    <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+    <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+      © 2024{" "}
+      <a href="/" className="hover:underline">
+        FinalStop
+      </a>
+      {/* <span>™</span> */}
+      . All Rights Reserved.
+    </span>
   </div>
 </footer>
 
